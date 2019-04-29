81f8a945a433bd34afd297cb81db597e3006cd53
Audra Tallman <audi.tallman@gmail.com>
2019-04-29 10:26:50 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,16 +1,16 @@
+
 require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-
-  get '/' do 
+  get '/' do
     erb :index
-  end 
-  
-  get '/new' do 
+  end
+
+  get '/new' do
     erb :create_puppy
-  end 
-  
+  end
+
   post '/puppy' do
     @puppy = Puppy.new(params['name'], params['breed'], params['months_old'])
     erb :display_puppy
