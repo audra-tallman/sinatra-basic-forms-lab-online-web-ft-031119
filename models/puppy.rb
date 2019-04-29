bb7b3ea11d0934819f5790de7cdc151c083004e3
Audra Tallman <audi.tallman@gmail.com>
2019-04-29 10:08:52 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,15 +1,11 @@
 class Puppy
-  attr_accessor :name, :age
+  attr_accessor :name, :months_old
   attr_reader :breed
   
-  def initialize (name, breed, age)
+  def initialize (name, breed, months_old)
     @name= name
     @breed= breed
-    @age= months_old
+    @months_old= months_old
   end 
-  
-  def months_old
-    
-  end 
-  
+
 end 
